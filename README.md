208c79c2758ecc1db4b52fc35c9493aed373ee18
nitin1291 <nitin1291@gmail.com>
2020-07-29 16:30:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-intl-tel-input-demo-txudvw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-intl-tel-input-demo-txudvw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SearchCountryField, TooltipLabel, CountryISO } from 'ngx-intl-tel-input';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	separateDialCode = true;
+	SearchCountryField = SearchCountryField;
+	TooltipLabel = TooltipLabel;
+	CountryISO = CountryISO;
+	preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
+	phoneForm = new FormGroup({
+		phone: new FormControl(undefined, [Validators.required])
+	});
+
+	changePreferredCountries() {
+		this.preferredCountries = [CountryISO.India, CountryISO.Canada];
+	}
+}
